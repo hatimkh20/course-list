5c1d5e7478ba92b0de496aabae45449c0f2e6c99
hatimkh20 <hatimk51@gmail.com>
2023-02-15 23:48:04 +0500

    unused imports omit

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,9 +1,7 @@
 import { ActionReducerMap, combineReducers } from '@ngrx/store';
 import { BookState } from '../books/models/state.model';
-import { bookReducers } from '../books/reducers';
 import { bookReducer } from '../books/reducers/book.reducer';
 import { CourseState } from '../course/models/state.model';
-import { courseReducers } from '../course/reducers';
 import { courseReducer } from '../course/reducers/course.reducer';
 
 export interface State {
@@ -15,8 +13,3 @@ export const reducers: ActionReducerMap<State, any> = {
   courseList: courseReducer,
   bookList: bookReducer
 };
-
-// export const rootReducer = combineReducers({
-//   bookList: bookReducers.bookList,
-//   courseList: courseReducers.courseList
-// });
